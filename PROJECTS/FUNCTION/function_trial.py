b2609e8a15002c3a51caa0cb2c4749d02948faba
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-12 22:27:16 +0530

    addition of number in function via user values.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,7 +1,10 @@
 def sum (a, b, c):
-    
     sum = a + b + c
-    return sum
+    return sum  
 
-value = sum(10, 20, 30)
-print(value)
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+value = sum(a, b, c)
+print("The sum of these three number is : ", value)
