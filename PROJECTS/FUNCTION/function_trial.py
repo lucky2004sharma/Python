b6425bb6845351fc9df317f29cd4c936e9f4ebb6
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-12 22:22:27 +0530

    function by return .

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,8 +1,6 @@
-def greet(name):
-    
-    print("Hello, " + name + "! Welcome to the company.")
-    
 
-greet("John Doe")
+def greet (name):
+    return f"Hello, {name}! Welcome to the company."
 
-
+name1 = greet("John Doe")
+print(name1)
