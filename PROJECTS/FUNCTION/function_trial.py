ad05c5bd8a8ea183b695232b8223c577b0521229
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-16 00:00:09 +0530

    hii

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,47 +1,6 @@
-# print("Welcome to temperature converter")
+def chai_banao(doodh, chini):
+    chai = doodh + chini   # ye 'chai' sirf yahin ke andar hai
+    return chai              # isse bahar bhej rahe hain
 
-# def celcius_to_farenheit(celcius):
-#     farenheit = ( celcius * 9/5 ) + 32
-#     return farenheit
-
-# def farenheit_to_celcius(farenheit):
-#     celcius = ( farenheit - 32 ) * 5/9
-#     return celcius
-
-
-
-
-# print("What you want to convert ?")
-# print(" 1. Celcius to farenheit : ")
-# print(" 2. Farenheit to celcius : ")
-
-
-# choice = int(input("Please select the value 1 or 2: "))
-
-# if choice == 1:
-#     farenheit = celcius_to_farenheit(celcius)
-# elif choice == 2:
-#     celcius = farenheit_to_celcius(farenheit)
-
-
-
-# faren = celcius_to_farenheit(celcius)
-# celci = farenheit_to_celcius(farenheit)
-
-# print("The value of farenheit by celcius is : ", faren)
-# print("The value of celcius by farenheit is : ", celci)
-
-
-
-
-
-print("Welcome to temperature converter")
-
-def celcius_to_farenheit(celcius):
-    farenheit = ( celcius * 9/5 ) + 32
-    return farenheit
-
-def farenheit_to_celcius(farenheit):
-    celcius = ( farenheit - 32 ) * 5/9
-    return celcius
-
+chai_banao(2, 1)
+print(chai)   # ERROR — kyunki 'chai' bahar hai hi nahi
