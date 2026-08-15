99e95e690b3dff24b7192155b9dc0347994bfb95
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-15 23:26:48 +0530

    updation.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,4 +1,39 @@
-    # fahrenheit = (celsius * 9 / 5) + 32
+# print("Welcome to temperature converter")
+
+# def celcius_to_farenheit(celcius):
+#     farenheit = ( celcius * 9/5 ) + 32
+#     return farenheit
+
+# def farenheit_to_celcius(farenheit):
+#     celcius = ( farenheit - 32 ) * 5/9
+#     return celcius
+
+
+
+
+# print("What you want to convert ?")
+# print(" 1. Celcius to farenheit : ")
+# print(" 2. Farenheit to celcius : ")
+
+
+# choice = int(input("Please select the value 1 or 2: "))
+
+# if choice == 1:
+#     farenheit = celcius_to_farenheit(celcius)
+# elif choice == 2:
+#     celcius = farenheit_to_celcius(farenheit)
+
+
+
+# faren = celcius_to_farenheit(celcius)
+# celci = farenheit_to_celcius(farenheit)
+
+# print("The value of farenheit by celcius is : ", faren)
+# print("The value of celcius by farenheit is : ", celci)
+
+
+
+
 
 print("Welcome to temperature converter")
 
@@ -10,25 +45,3 @@ def farenheit_to_celcius(farenheit):
     celcius = ( farenheit - 32 ) * 5/9
     return celcius
 
-
-
-
-print("What you want to convert ?")
-print(" 1. Celcius to farenheit : ")
-print(" 2. Farenheit to celcius : ")
-
-
-choice = int(input("Please select the value 1 or 2: "))
-
-if choice == 1:
-    farenheit = celcius_to_farenheit(celcius)
-elif choice == 2:
-    celcius = farenheit_to_celcius(farenheit)
-
-
-
-faren = celcius_to_farenheit(celcius)
-celci = farenheit_to_celcius(farenheit)
-
-print("The value of farenheit by celcius is : ", faren)
-print("The value of celcius by farenheit is : ", celci)
