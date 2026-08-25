b7aac16d1cf88988e9f35167d3e83bac8f210edc
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-25 22:22:55 +0530

    local variables.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,20 +1,9 @@
-class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance = balance
+country = "india"
 
-    def deposit(self, amount):
-        self.balance += amount
-        print(f"Deposited {amount}. New balance: {self.balance}")
-
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient funds.")
-        else:
-            self.balance -= amount
-            print(f"Withdrew {amount}. New balance: {self.balance}")
-
-account = BankAccount("Alex", 100)
-account.deposit(50)
-account.withdraw(30)
-account.withdraw(1000)
+def values(num):
+    num = num + 10
+    print(num)
+    
+values(5)
+print(country)
+    
