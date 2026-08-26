c52a09f9845c0f57c0c60b4484e8e0d96bfe25dc
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-26 22:56:57 +0530

    calculator of bank.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,19 +1,32 @@
-class Student:
+class BankAccount:
 
-    def __init__(self, name, age, course):
+    def __init__(self, name, balance):
         self.name = name
-        self.age = age
-        self.course = course
+        self.balance = balance
 
-    def show_details(self):
-        print("----- Student Details -----")
-        print("Name:", self.name)
-        print("Age:", self.age)
-        print("Course:", self.course)
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("₹", amount, "deposited successfully.")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance = self.balance - amount
+            print("₹", amount, "withdrawn successfully.")
+        else:
+            print("Insufficient balance!")
+
+    def show_balance(self):
+        print("Account Holder:", self.name)
+        print("Current Balance: ₹", self.balance)
 
 
-# Creating object
-student1 = Student("Rahul", 20, "Python")
+# Create account
+account1 = BankAccount("Mohit", 5000)
 
-# Calling method
-student1.show_details()
+account1.show_balance()
+
+account1.deposit(2000)
+
+account1.withdraw(1500)
+
+account1.show_balance()
