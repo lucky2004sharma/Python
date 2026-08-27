9e646b35d6178348dbc3a2462ad91595c30bd961
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-27 18:17:49 +0530

    checking memory allocation.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,24 +1,7 @@
-class Calculator:
-
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            return "Cannot divide by zero"
-        return a / b
-
-
-# Create object
-calc = Calculator()
-
-print("Addition:", calc.add(10, 5))
-print("Subtraction:", calc.subtract(10, 5))
-print("Multiplication:", calc.multiply(10, 5))
-print("Division:", calc.divide(10, 5))
+def value():
+    print("Hello, World!")
+    
+# value()
+# valu = value()
+# print(valu)
+print(value)
