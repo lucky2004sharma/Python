9283175ef90c848100d98921282cb5efe92dd536
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-27 22:45:34 +0530

    nested function loop.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -4,6 +4,11 @@ def outer():
     def inner():
         print("world")
         
+        def inner_inner():
+            print("welcome")
+            
+        inner_inner()
+        
     inner()
 
 
