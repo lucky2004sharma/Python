e75efd7f6ffff48f8fa22e2a9dfe0ac06b67393f
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-29 23:19:13 +0530

    update.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,8 +1,25 @@
-def display (item, currency):
+# def display (item, currency):
     
+#     total = sum(item.values())
+#     print("Expenses are :", total, currency)
+    
+    
+# values = {"Groceries": 150.0, "Transport": 50.0, "Entertainment": 100.0}
+# display (values, "USD")
+
+
+def display(item, currency):
     total = sum(item.values())
-    print("Expenses are :", total, currency)
+    if total > 100:
+        print("Expenses are:", total, currency)
+        
+    elif total >200:
+        print("Expenses are:", total, currency)
+    
+    else:
+        print("Expenses are:", total, currency)
     
     
-values = {"Groceries": 150.0, "Transport": 50.0, "Entertainment": 100.0}
-display (values, "USD")
+car = {"Fuel": 100.0, "Maintenance": 50.0, "Insurance": 200.0}
+display(car, "USD")
+
