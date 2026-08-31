086f8e8a916c2a6ebfc0f417d4bbc66019c148bd
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-31 15:18:30 +0530

    varaible keyword.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -4,12 +4,24 @@
 # # addition(10,20)
 # print(addition(10,20))
 
-def addition(*nums):
-    sum = 0
-    for n in nums:
-        sum = sum + n 
-    print(sum)
-    return sum
 
-addition(10,20,30)
-addition(10,20,30,40,50,60)
+# variable length positional arguments.
+# def addition(*nums):
+#     sum = 0
+#     for n in nums:
+#         sum = sum + n 
+#     print(sum)
+#     return sum
+
+# addition(10,20,30)
+# addition(10,20,30,40,50,60)
+
+# variable length keyword argumengs.
+
+def addition(**nums):
+    print(nums)
+    print(type(nums))
+    return sum(nums.values())
+
+addition(a=20, b=30, c=40)
+print(addition(a=10,b=20,c=30,d=40))
