b8750e57311405a04c818dc25050e2ef524d9022
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-31 14:38:14 +0530

    updated for string.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,29 +1,10 @@
-# def items(name, employee= []):
-#     employee.append(name)
-#     print("update value is :", employee)
-#     # return employee
-
-
-# add_employee = items("John")
-# print(items.__defaults__)
-# add_employee = items("Jane")
-# print(items.__defaults__)
-# add_employee = items("Doe")
-# print(items.__defaults__)
-
-def items(name, employee1=None):
-    if employee1 is None:
-        employee1 = []
-        # employee1 = [name]
-
-    employee1.append(name)
+def items(name, employee1 = "yoyo"):
+    employee1 = employee1 + " " + name
     print("update value is :", employee1, name)
     
 items("John")
-print(items.__defaults__)
+print(items.__defaults__ )
+items("John", "lucifer")
+items("John")
+items("John")
 
-add_employee = items("Jane")
-print(items.__defaults__)           
-
-add_employee = items("Doe", ["Alice", "Bob"])
-print(items.__defaults__)
