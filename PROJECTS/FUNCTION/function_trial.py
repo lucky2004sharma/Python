b5d7f34e7f9926e2e2ba1358bf0543f1789336c8
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-31 14:15:59 +0530

    mutuable list bug.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,9 +1,12 @@
 def items(name, employee= []):
     employee.append(name)
-    print(employee)
-    return employee
+    print("update value is :", employee)
+    # return employee
 
 
 add_employee = items("John")
+print(items.__defaults__)
 add_employee = items("Jane")
+print(items.__defaults__)
 add_employee = items("Doe")
+print(items.__defaults__)
