44e4bdb7e3774424fb89ac2b653d015342fa6fb7
lucky2004sharma <lucky11sharma05@gmail.com>
2026-08-31 14:23:53 +0530

    mutuable bugs .

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,12 +1,29 @@
-def items(name, employee= []):
-    employee.append(name)
-    print("update value is :", employee)
-    # return employee
+# def items(name, employee= []):
+#     employee.append(name)
+#     print("update value is :", employee)
+#     # return employee
 
 
-add_employee = items("John")
+# add_employee = items("John")
+# print(items.__defaults__)
+# add_employee = items("Jane")
+# print(items.__defaults__)
+# add_employee = items("Doe")
+# print(items.__defaults__)
+
+def items(name, employee1=None):
+    if employee1 is None:
+        employee1 = []
+        # employee1 = [name]
+
+    employee1.append(name)
+    print("update value is :", employee1)
+    
+items("John")
 print(items.__defaults__)
+
 add_employee = items("Jane")
-print(items.__defaults__)
-add_employee = items("Doe")
+print(items.__defaults__)           
+
+add_employee = items("Doe", ["Alice", "Bob"])
 print(items.__defaults__)
