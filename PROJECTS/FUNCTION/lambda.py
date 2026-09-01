7ba1ebb654b4befb528e335f87f88cdc691f4e59
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-01 12:04:34 +0530

    mutliptle type and usage of lambda.

--- PROJECTS/FUNCTION/lambda.py
+++ PROJECTS/FUNCTION/lambda.py
@@ -1,8 +1,19 @@
-# def addition(a, b):
-#     return a + b
+add = lambda a, b : a+b
+print(add(5, 3))  # Output: 8      normal lambda function 
 
-# print(addition(10, 20))    # This are normal function 
 
-#  LAMBDA FUNCTION
+add = lambda a, b=5 : a+b
+print(add(5))  # Output: 10     lambda function with default argument
 
-print(lambda a,b : a+b)    # This is lambda function   and print the memory location of the function
+
+add = lambda a, b=5, c=10 : a+b+c
+print(add(a =5))  # Output: 20     lambda function with default argument and keyword argument
+
+# add = lambda a, b: a+b
+# print(add(a=5, 5))  # Output: 10     lambda function with keyword argument and positional argument
+# # SyntaxError: positional argument follows keyword argument
+
+
+# num = lambda a,b : a+b, a-b
+# print(num(5, 3))  # Output: (8, 2)     lambda function with multiple return values
+# NameError: name 'a' is not defined
