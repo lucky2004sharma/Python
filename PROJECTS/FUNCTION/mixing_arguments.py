71f48bf7efa9f513aa4cfa200596726316b39c89
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-01 11:46:53 +0530

    mixing arguments.

--- PROJECTS/FUNCTION/mixing_arguments.py
+++ PROJECTS/FUNCTION/mixing_arguments.py
@@ -4,4 +4,10 @@ def info(name, age):
     print(f"My name is {name} and my age is {age}")
     
 
-info('rahul', 21)
+info('rahul', 21)    # both are positional arguments
+
+info(name = 'shyam', age = 22)    # both are keywords arguments
+
+info('rahul', age = 23)     # positional --> keywords    work fine
+
+# info(name = 'shyam', 24)    # keyword--> positional      error cause
