7572eeb49dae8568dcefddc17741302fd99b8022
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-01 11:50:50 +0530

    lambda function.

---
+++ PROJECTS/FUNCTION/lambda.py
@@ -0,0 +1,8 @@
+# def addition(a, b):
+#     return a + b
+
+# print(addition(10, 20))    # This are normal function 
+
+#  LAMBDA FUNCTION
+
+print(lambda a,b : a+b)    # This is lambda function   and print the memory location of the function
