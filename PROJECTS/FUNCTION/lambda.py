614d61fcc60c44a607f759d3b04bbe261a7d3475
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-01 15:26:03 +0530

    nested lambda.

--- PROJECTS/FUNCTION/lambda.py
+++ PROJECTS/FUNCTION/lambda.py
@@ -1,36 +1,66 @@
-'''# WHY USE THE LAMBDA FUNCTION?
+# '''# WHY USE THE LAMBDA FUNCTION?
 
-# print(sorted([5,7,65,89,66,4,22,55]))
+# # print(sorted([5,7,65,89,66,4,22,55]))
 
-data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
+# data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
 
-sorted_data = sorted(data)
-print(sorted_data) # sorted in alphabetical order
-
-
-sorted_data = sorted(data, key=len)
-print(sorted_data) # sorted in ascending order of length of string
-
-# split funciton
-print('sharma rohit'.split( )) # output = ['sharma', 'rohit']  # split the string into list of words
-print('sharma rohit'.split()[1])  # ['sharma'=0, 'rohit'=1]  # output = rohit'''
+# sorted_data = sorted(data)
+# print(sorted_data) # sorted in alphabetical order
 
 
-# def sort_by_last_name(name1):
+# sorted_data = sorted(data, key=len)
+# print(sorted_data) # sorted in ascending order of length of string
+
+# # split funciton
+# print('sharma rohit'.split( )) # output = ['sharma', 'rohit']  # split the string into list of words
+# print('sharma rohit'.split()[1])  # ['sharma'=0, 'rohit'=1]  # output = rohit'''
+
+
+# # def sort_by_last_name(name1):
+# #     first_name = name1.split()[1]
+# #     return first_name
+    
+# # data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
+# # print(sorted(data, key=sort_by_last_name)) # sorted in ascending order of last name
+
+# # by lambda function
+
+# # data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
+# # print(sorted(data, key = lambda name : name.split()[1])) # sorted in ascending order of last name
+
+# def change_name(name1):
 #     first_name = name1.split()[1]
 #     return first_name
-    
-# data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
-# print(sorted(data, key=sort_by_last_name)) # sorted in ascending order of last name
-
-# by lambda function
 
 # data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
-# print(sorted(data, key = lambda name : name.split()[1])) # sorted in ascending order of last name
+# print(sorted(data, key = change_name)) # sorted in ascending order of last name
 
-def change_name(name1):
-    first_name = name1.split()[1]
-    return first_name
 
-data = ['tendulkar sachin', 'kohli virat', 'dravid rahul', 'dhoni ms', 'sehwag virender']
-print(sorted(data, key = change_name)) # sorted in ascending order of last name
+
+
+
+def outer(): # by normal function
+    def add(a, b):
+        return a + b
+    return add
+
+
+additon = outer()
+print(additon(5, 6)) 
+
+def outer(): # by lambda function
+    add = lambda a, b : a + b
+    return add
+
+addition = outer()
+print(addition(5, 6))
+
+# nested lambda function
+
+outer = lambda : (lambda a, b : a + b)
+addition = outer()  
+print(addition(5, 10))
+
+outer =  lambda a, b : a + b
+
+print(outer(5, 10))
