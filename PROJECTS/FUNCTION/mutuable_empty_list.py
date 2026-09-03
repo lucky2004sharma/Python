7cec8de29e81d174f976a3d9c3112e392924cfdf
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-03 13:33:37 +0530

    fileter function.

--- PROJECTS/FUNCTION/mutuable_empty_list.py
+++ PROJECTS/FUNCTION/mutuable_empty_list.py
@@ -1,17 +1,17 @@
-def get_name():
-    first_name = input("Enter your first name : ")
-    last_name = input("Enter your last name : ")
-    return first_name + " " + last_name
+data = [5,10,15,20,25,30]
 
-get_name = get_name()
-print(get_name)
-
-
-def get_name(first_name, last_name):
-    return first_name + " " + last_name
+def even_num(data):
+    if data % 2 == 0:
+        return True
+    else:
+        return False
     
-    
+filtered_data = filter(even_num, data)
 
-first_name = input("Enter your first name : ")
-last_name = input("Enter your last name : ")
-print(get_name(first_name, last_name))
+print(filtered_data) # 0<filter object at 0x0000020D9A1B7C10>
+print(type(filtered_data)) # <class 'filter'>
+# for element in filtered_data:
+#     print(element) # 10, 20, 30
+
+print(list(filtered_data)) # []  # filter object is exhausted after first iteration, so it returns empty list
+# print(list(filtered_data())) # []  # filter object is exhausted after first iteration, so it returns empty list  
