cecb80485852ab7dcc3a909c0f0596a3f8645fe7
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-03 18:05:13 +0530

    completley map funciton.

--- PROJECTS/FUNCTION/map_fuction().py
+++ PROJECTS/FUNCTION/map_fuction().py
@@ -1,4 +1,4 @@
-name = ['om', 'jay','shiv','rahul','sachin']
+'''name = ['om', 'jay','shiv','rahul','sachin']
 def check_len(name):
     # return len(name) # only print the length of each name in the list
     # return name # only print the name in the list
@@ -13,6 +13,36 @@ for element in lengths:
     # print(element) # print the name and length of each name in the list
     print(element[0], element[1]) # print the name and length of each name in the list
     
+    
+# print square of each number in the list
 
+numbers = [1, 2, 3, 4, 5]
+def square(num):
+    return num ** 2
+
+squared_numbers = map(square, numbers)
+# print(list(squared_numbers)) # print the square of each number in the list
+for element in squared_numbers:
+    print(element)
     
     
+# by lambda function
+
+numbers = [ 2, 3, 4, 5, 6]
+
+squared_number = map( lambda num : num ** 2, numbers)
+print(list(squared_number))'''
+
+
+
+numbers = [1, 2, 3, 4, 5]
+def square(num):
+    if num % 2 != 0:
+        return num ** 2
+    else:
+        return num
+
+squared_numbers = map(square, numbers)
+print(list(squared_numbers)) # print the square of each number in the list
+# for element in squared_numbers:
+#     print(element)
