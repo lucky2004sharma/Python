40942a459aa41e2e45b13e3fe9a94ea3428ce785
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-04 13:04:26 +0530

    partial funciton.

--- PROJECTS/FUNCTION/reduce_function.py
+++ PROJECTS/FUNCTION/reduce_function.py
@@ -19,15 +19,21 @@ data = [ 1,2,3,4,5 ]
 full_sum = functools.reduce(lambda a, b : a+b, data)
 print(full_sum)'''
 
-import functools
+from functools import partial
 
-data = [1,2,55,4,5]
+# data = [1,2,55,4,5]
 
-def max(a, b):
-    if a > b:
-        return a
-    else:
-	    return b
+# def max(a, b):
+#     if a > b:
+#         return a
+#     else:
+# 	    return b
 
-max_number = functools.reduce(max, data)
-print(max_number)
+# max_number = functools.reduce(max, data)
+# print(max_number)
+
+def addition(a,b,c,d):
+    return a + b + c + d
+
+value = partial(addition, a =5, b =10)
+print(value(c=15, d=20)) # output = 50
