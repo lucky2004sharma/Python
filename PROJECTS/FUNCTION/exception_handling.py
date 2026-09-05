682c44295ef3b15f8c1338e3dec9be122e1ca4f6
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-05 17:28:33 +0530

    exception handiling.

---
+++ PROJECTS/FUNCTION/exception_handling.py
@@ -0,0 +1,8 @@
+def multiply(num1, num2):
+    return num1 * num2
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+multiplied_value = multiply(num1, num2)
+print("The multiplication of", num1, "and", num2, "is:", multiplied_value)
