67ea5a5400d7b01e7c96025c4350273f42e5a214
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-07 15:59:42 +0530

    divide by 5 in exception.

--- PROJECTS/FUNCTION/exception_handling.py
+++ PROJECTS/FUNCTION/exception_handling.py
@@ -1,8 +1,11 @@
 # exception example number should not be divisible by 5
 
 class FiveDivisionError(Exception):
-    
+    # this is exception class called when number is try to divide by 5
+    # def __init__(self):
+    #     print("cannot divide by 5")  # just for information
     pass
+
 
 n1 = int(input("Enter the 1st number "))
 n2 = int(input("Enter the 2nd number "))
