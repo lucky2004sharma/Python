ed41fd1834dfde6dc044cd2039a73a9e96854550
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-07 15:20:52 +0530

    raise exception.

--- PROJECTS/FUNCTION/exception_handling.py
+++ PROJECTS/FUNCTION/exception_handling.py
@@ -1,10 +1,10 @@
 try :
-    
-    age = int(input("Enter your age: "))
-    if age < 0:
-        raise ValueError
+    age = int(input("Enter your age. "))
+    if age<0:
+       raise ValueError
     print("your age is ", age)
     
-except ValueError :
-    print("Enter valid age ")
+except ValueError:
+    print("enter valid age.")
     
+print("stop")
