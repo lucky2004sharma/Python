559746cfc980657f52c95a569bec43bd8d802809
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-07 17:28:35 +0530

    atm via functions.

--- PROJECTS/FUNCTION/exception_handling.py
+++ PROJECTS/FUNCTION/exception_handling.py
@@ -1,24 +1,55 @@
-# exception example number should not be divisible by 5
+# ATM MACHINE WITH TRY AND EXCEPTION.
 
-class FiveDivisionError(Exception):
-    # this is exception class called when number is try to divide by 5
-    # def __init__(self):
-    #     print("cannot divide by 5")  # just for information
-    pass
+def password():
+    user_pin = 1234
+    pin = int(input("Please enter the pin : ", pin))
+    if user_pin == pin:
+        return 'welcome '
+    else:
+        return 'Wrong PIN : '
+    
+def withdraw(balance,user_withdraw):
+    # print("How much money you want to withdraw : ")
+    if user_withdraw>=10000:
+        return "insuficient fund"
+    elif user_withdraw>=9000:
+        return 'At least one thousanbd should be in account'
+    else:
+        after_withdraw = balance - user_withdraw
+        return after_withdraw
+    
+def deposit(balance,user_deposit):
+    print("How much money you want to deposit : ")
+    after_deposit = balance + user_deposit
+    return after_deposit
 
 
-n1 = int(input("Enter the 1st number "))
-n2 = int(input("Enter the 2nd number "))
-try:
-    if n2 == 5:
-        raise FiveDivisionError("Doesn't by 5")
-    div = n1/n2
-    print("Division is ", div)
-    
-except (FiveDivisionError, ZeroDivisionError) as var :
-    print("error :", var)
-    
-print("Stop")
+print("----------- WELCOME TO ATM ----------")
 
+
+while True:
+    print("1. Check balance")
+    print("2. Withdraw Money")
+    print("3. Deposit Money")
+    balance = 10000
     
-    
+    choice = int(input("Choose what you want to search : "))
+    if choice == 1:
+        print("Thank you for checking the balance : ", balance)
+        
+    elif choice ==2:
+        user_withdraw = int(input("How much money you want to withdraw Please enter the amount : "))
+        withdraw_amount = withdraw(balance,user_withdraw)
+        print("Message : ", withdraw_amount)
+        
+    elif choice == 3:
+        user_deposit = int(input("Enter the amount to deposit : "))
+
+        deposit_amount= deposit(balance,user_deposit)
+        print("Money after deposit : ", deposit_amount)
+        
+        
+    else:
+        print("choose between 1, 2 and 3")
+        break
+        
