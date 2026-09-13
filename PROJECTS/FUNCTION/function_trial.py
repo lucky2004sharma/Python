4cc1ee1d8a5b726a1104b52a6faef68063cdad1f
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-13 22:47:40 +0530

    value generator.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,7 +1,4 @@
-num = int(input("Enter a number: "))
-steps = [num]
-while num >= 10:
-    num = sum(int(d) for d in str(num))
-    steps.append(num)
-print(" -> ".join(map(str, steps)))
-print("Digital root:", steps[-1])
+import random
+adjectives = ["brilliant", "creative", "kind", "resilient", "unstoppable"]
+nouns = ["human", "thinker", "friend", "creator", "explorer"]
+print(f"You are a {random.choice(adjectives)} {random.choice(nouns)}!")
