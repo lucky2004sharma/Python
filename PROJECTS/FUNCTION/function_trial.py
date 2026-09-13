20d869a1f40cda0977400716e12e8eee3cd1f3cb
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-13 22:36:59 +0530

    rot value.

--- PROJECTS/FUNCTION/function_trial.py
+++ PROJECTS/FUNCTION/function_trial.py
@@ -1,12 +1,7 @@
-def square():
-    try :
-        num = int(input("Print enter the number : "))
-        print("Square of number is : ", num**2)
-        
-    except Exception as value:
-        print("Error :", value) 
-        square()
-        
-square()
-        
-print("Yo")
+num = int(input("Enter a number: "))
+steps = [num]
+while num >= 10:
+    num = sum(int(d) for d in str(num))
+    steps.append(num)
+print(" -> ".join(map(str, steps)))
+print("Digital root:", steps[-1])
