daad25457bbc34af8552a45ad234ddb8c4fe2f1e
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-15 11:49:21 +0530

    file handler

--- PROJECTS/FILE HANDLING/file_01.py
+++ PROJECTS/FILE HANDLING/file_01.py
@@ -1,9 +1,3 @@
-hours = int(input("Hours until your deadline: "))
-minutes = hours * 60
-messages = {
-    0: "It's already here — go!",
-    60: "Only an hour, hustle!",
-    180: "A few hours, stay focused!",
-}
-closest = min(messages.keys(), key=lambda x: abs(x - minutes))
-print(f"You have {minutes} minutes left. {messages[closest]}")
+file = open('testing.txt', 'r')
+value = file.read()
+print(value)
