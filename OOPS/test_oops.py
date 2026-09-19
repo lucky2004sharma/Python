662e7b9c0b3f9c7308fc3a78adb9326bd64150a5
lucky2004sharma <lucky11sharma05@gmail.com>
2026-09-19 22:19:11 +0530

    update code

--- OOPS/test_oops.py
+++ OOPS/test_oops.py
@@ -1,22 +1,7 @@
-# Python Program to find the L.C.M. of two input number
+number = int(input("Enter a number: "))
+for i in range(1, 6):
 
-def compute_lcm(x, y):
-
-   # choose the greater number
-   if x > y:
-       greater = x
-   else:
-       greater = y
-
-   while(True):
-       if((greater % x == 0) and (greater % y == 0)):
-           lcm = greater
-           break
-       greater += 1
-
-   return lcm
-
-num1 = 54
-num2 = 24
-
-print("The L.C.M. is", compute_lcm(num1, num2))
+    # Terminate the loop if i equals number
+    if i == number:
+        break
+    print(i)
